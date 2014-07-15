c38f22d9784511472411dbb1c575e50bf62b6e1c
Maks Materkov <materkov@gmail.com>
2014-07-15 20:13:16 +0600

    IM

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var http = require('http'),
     config = require('./config.js');
 
 var LONG_POOL_TIMEOUT = 25 * 1000; // 25 seconds
-var EVENTS_EXPIRE_TIME = 60 * 1000; // 60 seconds
+var EVENTS_EXPIRE_TIME = 20 * 60 * 1000; // 20 minutes
 
 var events = [];
 var pending = [];
@@ -93,9 +93,18 @@ function addEvent(event, data) {
   checkPendingForEvent(_event);
 }
 
-function writeBadRequest(res) {
-  res.writeHead(400);
-  res.write('Bad Request');
+function writeError(res, error) {
+  res.writeHead(error, {
+    "Access-Control-Allow-Origin": "*",
+  });
+  
+  if (error == 400)
+    res.write('Bad Request');
+  else if (error = 403)
+    res.write('Access denied');
+  else if (error = 502)
+    res.write('Something wrong');
+  
   res.end();
 }
 
@@ -115,14 +124,14 @@ function listenerAPI(req, res) {
     req.on('end', function () {
       var post = qs.parse(body);
       if (!post.event || !post.data) {
-        writeBadRequest(res);
+        writeError(res, 400);
       };
 
       try {
         var parsedData = JSON.parse(post.data);
       }
       catch (e) {
-        writeBadRequest(res);
+        writeError(res, 400);
         return;
       }
 
@@ -144,6 +153,13 @@ function checkAccess(authToken, requestedEvents) {
       'instantmessage.' + authToken.app + '.' + authToken.user,
     ];
   }
+  else if (authToken.type == 'panel') {
+    // Этот тукен может подписаться на: message.APPID
+    var allowedEvents = [];
+
+    for (var i = 0; i < authToken.apps.length; i++) 
+      allowedEvents.push('instantmessage.' + authToken.apps[i]);
+  }
 
 
   // Проверим что все события, на которые подписывается - разрешены
@@ -151,12 +167,10 @@ function checkAccess(authToken, requestedEvents) {
   var i = 0, j = 0;
 
   for (i = 0; i < requestedEvents.length; i++) {
-    for (j = 0; j < allowedEvents.length; j++) {
-      if (allowedEvents[j] != requestedEvents[i]) {
-        isValid = false;
-        break;
-      }
-    }
+    if (allowedEvents.indexOf(requestedEvents[i]) == -1) {
+      isValid = false;
+      break;
+    };
   }
 
   return isValid;
@@ -165,10 +179,11 @@ function checkAccess(authToken, requestedEvents) {
 function listenerClient(req, res) {
   var u = url.parse(req.url, true);
   if (!u.query || !u.query.events || !u.query.auth_token) {
-    writeBadRequest(res);
+    writeError(res, 400);
     return;
   }
 
+  u.query.timestamp = parseInt(u.query.timestamp);  // Force integer convert
   if (!u.query.timestamp)
     u.query.timestamp = currentTimestamp();
 
@@ -177,7 +192,7 @@ function listenerClient(req, res) {
       body = JSON.parse(body);
     }
     catch (e) {
-      body = {meta: {status: 502}};
+      body = {meta: {status: 502}, data: {}};
     }
 
     if (body.meta.status == 200 && body.data.active) {
@@ -187,9 +202,7 @@ function listenerClient(req, res) {
 
       if (!checkAccess(body.data, requestedEvents)) {
         // Пытается подписаться на неразрешенное событие
-        res.writeHead(403);
-        res.write('Access Denied');
-        res.end();
+        writeError(res, 403);
         return;
       }
 
@@ -200,8 +213,9 @@ function listenerClient(req, res) {
       for (i = 0; i < events.length; i++) {
         if (events[i].timestamp > u.query.timestamp) {
           for (j = 0; j < requestedEvents.length; j++) {
-            if (events[i].event.startsWith(requestedEvents[j]))
+            if (events[i].event.startsWith(requestedEvents[j])) {
               immediateReturn.push(events[i]);
+            }
           }
         }
       }
@@ -228,15 +242,12 @@ function listenerClient(req, res) {
     }
     else if (body.meta.status == 403 || !body.data.active) {
       // Неверный или неактивный auth token
-      res.writeHead(403);
-      res.write('Access Denied');
-      res.end();
+      writeError(res, 403);
     }
     else {
       // Something wrong
       res.writeHead(502);
-      res.write('Something wrong');
-      res.end();
+      writeError(res, 502);
     }
   });
 }
